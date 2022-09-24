ab3c806e3af706c76882a737ed4880f6238db2ba
Alex <seredin-alex@mail.ru>
2022-09-24 16:32:47 +0300

    Добавил задание 6

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("Значение переменной fitnesMan - с типом float (с плавающей запятой) равно " + fitnesMan);
         double dixiSpecialSix = 46.1234567891123456;
         System.out.println("Значение переменной dixiSpecialSix - с типом double (с плавающей запятой) равно " + dixiSpecialSix);
+
         System.out.println("Задание 2");
         float f = 27.12f;
         System.out.println("Значение " + f + " - переменная float");
@@ -33,12 +34,14 @@ public class Main {
         System.out.println("Значение " + house4 + " - переменная short" );
         byte bigFut = 67;
         System.out.println("Значение " + bigFut + " - переменная byte" );
+
         System.out.println("Задание 3");
         byte teacherLuda = 23;
         byte teacherAnna = 27;
         byte teacherCaterina = 30;
         short total = 480;
         System.out.println("На каждого ученика рассчитано " + total / (teacherLuda + teacherAnna + teacherCaterina) + " листов бумаги" );
+
         System.out.println("Задание 4");
         byte bootle = 16;
         byte timeMinut = 2;
@@ -52,13 +55,21 @@ public class Main {
         System.out.println("За 3 дня машина произвела " + (bootle / timeMinut) * timeDay3 + " штук бутылок");
         int timeMonth = timeDay3 *10;
         System.out.println("За 1 месяц машина произвела " + (bootle / timeMinut) * timeMonth + " штук бутылок");
+
         System.out.println("Задание 5");
         byte totalWhiteBrown = 120;
         byte classWhite = 2;
         byte classBrown =4;
         int totalClass = totalWhiteBrown / (classWhite + classBrown);
         System.out.println("В школе, где " + totalClass + " классов, нужно " + totalClass * classWhite + " банок белой краски и " + totalClass * classBrown + " банок коричневой краски");
-        System.out.println("Задание 6");
 
+        System.out.println("Задание 6");
+        byte bananas = 80;
+        byte milk = 105;
+        byte iceCream = 100;
+        byte egg = 70;
+        double totalGramm = (5 * bananas) + (2 * milk) + (2 * iceCream) + (4 * egg);
+        System.out.println("Общий вес продуктов в спорт-завтраке составляет " + totalGramm + " грамм или ");
+        System.out.println(totalGramm / 1000 + " кг");
     }
 }
