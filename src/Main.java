3a5ef9093f15cb2cde3d240f10999b89fda36064
Alex <seredin-alex@mail.ru>
2022-09-24 08:39:55 +0300

    Задания с 1 по 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,53 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Урок 4 Переменные. часть 2");
         System.out.println("Задание 1");
-
+        int westernUnion = 2147483640;
+        System.out.println("Значение переменной westernUnion - с типом int (целочисленные) равно " + westernUnion);
+        byte sigalOne = -120;
+        System.out.println("Значение переменной sigalOne - с типом byte (целочисленные) равно " + sigalOne);
+        short xeniumTwoFerrum = 32760;
+        System.out.println("Значение переменной xeniumTwoFerrum - с типом short (целочисленные) равно " + xeniumTwoFerrum);
+        long vektorPlan = 9223372036854775805L;
+        System.out.println("Значение переменной vektorPlan - с типом long (целочисленные) равно " + vektorPlan);
+        float fitnesMan = 12.1234567f;
+        System.out.println("Значение переменной fitnesMan - с типом float (с плавающей запятой) равно " + fitnesMan);
+        double dixiSpecialSix = 46.1234567891123456;
+        System.out.println("Значение переменной dixiSpecialSix - с типом double (с плавающей запятой) равно " + dixiSpecialSix);
+        System.out.println("Задание 2");
+        float f = 27.12f;
+        System.out.println("Значение " + f + " - переменная float");
+        long l = 987678965549L;
+        System.out.println("Значение " + l + " - переменная long");
+        byte b = 2;
+        System.out.println("Значение " + b + " - переменная byte");
+        short h = 786;
+        System.out.println("Значение " + h + " - переменная short");
+        boolean bl = false;
+        System.out.println("Значение " + bl + " - переменная boolean");
+        short house2 = 786;
+        System.out.println("Значение " + house2 + " - переменная short" );
+        short house3 = -159;
+        System.out.println("Значение " + house3 + " - переменная short" );
+        short house4 = 27897;
+        System.out.println("Значение " + house4 + " - переменная short" );
+        byte bigFut = 67;
+        System.out.println("Значение " + bigFut + " - переменная byte" );
+        System.out.println("Задание 3");
+        byte teacherLuda = 23;
+        byte teacherAnna = 27;
+        byte teacherCaterina = 30;
+        short total = 480;
+        System.out.println("На каждого ученика рассчитано " + total / (teacherLuda + teacherAnna + teacherCaterina) + " листов бумаги" );
+        System.out.println("Задание 4");
+        byte bootle = 16;
+        byte timeMinut = 2;
+        byte time_Minut = 20;
+        System.out.println("За 20 минут машина произвела " + (bootle / timeMinut) * time_Minut + " штук бутылок");
+        int timeHours = time_Minut * 3;
+        System.out.println("За 1 час машина произвела " + (bootle / timeMinut) * timeHours + " штук бутылок");
+        int timeDay = timeHours * 24;
+        System.out.println("За 1 сутки машина произвела " + (bootle / timeMinut) * timeDay + " штук бутылок");
+        int timeDay3 = timeDay * 3;
+        System.out.println("За 3 дня машина произвела " + (bootle / timeMinut) * timeDay3 + " штук бутылок");
     }
 }
